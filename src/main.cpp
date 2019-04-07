2ac8fcc00f77c27b08d2819c56f84d2fc804aa35
3AMLABS\svenhe <sven.hecht@logmein.com>
2019-04-07 17:54:07 +0200

    Add button, refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,55 @@
 #include "Arduino.h"
 #include <Wire.h>
-#include <RTClib.h>
 #include <Adafruit_NeoPixel.h>
 
 #ifndef UNIT_TEST
 
 const String VERSION = "0.3";
 
-int PIXEL_PIN = 5;
+int BUTTON_PIN = 4;
 int MOISTURE_SENSOR_PIN = A0;
 int TANK_LEVEL_SENSOR_PIN = A1;
+int WATERING_AMOUNT_POTI_PIN = A2;
+int SENSITIVITY_POTI_PIN = A3;
+int PIXEL_PIN = 5;
 int VALVE_PIN = 7;
 
-TimeSpan CHECK_INTERVAL = TimeSpan(0, 0, 1, 0);
+const float CHECK_INTERVAL = 60000;
 const int WATER_SPREAD_DELAY = 5000;
-const int WATERING_AMOUNT_TIME = 1000;
+const int WATERING_AMOUNT_TIME = 3000;
 const int SCHEDULING_DELAY = 1500;
 const int EMPTY_TANK_CHECK_INTERVAL = 3000;
 const int MAX_WATERING_CYCLES = 10;
 const int CRITICAL_TANK_LEVEL = 100;
 
-const float MOISTURE_LEVEL_THRESHOLD = 0.5;
-
-const float MOISTURE_BASE_OFFSET = -250.0;
+const float MOISTURE_BASE_OFFSET = -320.0;
 const float MOISTURE_SCALE_FACTOR = 1/300.0;
 
-DateTime nextCheckDateTime;
+int wateringAmountTime = WATERING_AMOUNT_TIME;
+
+float nextCheckDateTime;
 bool tankLevelWasCritical;
+float moistureLevelThreshold = 0.5;
 
-RTC_DS1307 rtc;
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(1, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
-void printDate(const DateTime& dt) {
-    Serial.print(dt.year(), DEC);
-    Serial.print('/');
-    Serial.print(dt.month(), DEC);
-    Serial.print('/');
-    Serial.print(dt.day(), DEC);
-    Serial.print(' ');
-    Serial.print(dt.hour(), DEC);
-    Serial.print(':');
-    Serial.print(dt.minute(), DEC);
-    Serial.print(':');
-    Serial.print(dt.second(), DEC);
-    Serial.println();
-}
 
 void setStatusColor(int r, int g, int b) {
   pixel.setPixelColor(0, pixel.Color(r, g, b));
   pixel.show();
   delay(100);
+}
+
+void readMoistureLevelThreshold() {
+  int sensitivity = analogRead(SENSITIVITY_POTI_PIN);
+  float newMoistureLevelThreshold = sensitivity / 1024.0;
+
+  if (moistureLevelThreshold != newMoistureLevelThreshold)
+  {
+    moistureLevelThreshold = newMoistureLevelThreshold;
+
+    Serial.print("Adjust Sensitivity to ");
+    Serial.println(newMoistureLevelThreshold);
+  }
 }
 
 float readMoistureLevel() {
@@ -60,7 +60,7 @@ float readMoistureLevel() {
   Serial.print("[moisture level = ");
   Serial.print(result);
   Serial.print(" (Threshold is ");
-  Serial.print(MOISTURE_LEVEL_THRESHOLD);
+  Serial.print(moistureLevelThreshold);
   Serial.println(")]");
 
   return result;
@@ -79,7 +79,7 @@ bool tankLevelIsCritical() {
 }
 
 bool plantNeedsWater() {
-  return readMoistureLevel() < MOISTURE_LEVEL_THRESHOLD;
+  return readMoistureLevel() < moistureLevelThreshold;
 }
 
 void showEmptyTankWarning() {
@@ -89,15 +89,16 @@ void showEmptyTankWarning() {
     setStatusColor(0, 0, 0);
     delay(1000);
   }
-    setStatusColor(255, 0, 0);
+
+  setStatusColor(255, 0, 0);
 }
 
 void hideEmptyTankWarning() {
   setStatusColor(0, 255, 0);
 }
 
-TimeSpan getTimeUntilNextCheck() {
-  return nextCheckDateTime - rtc.now();
+float getTimeUntilNextCheck() {
+  return nextCheckDateTime - millis();
 }
 
 void openWaterValve(int durationMilliseconds) {
@@ -126,6 +127,23 @@ void checkTank() {
   }
 }
 
+void manualAdjustWateringAmount() {
+  int potiValue = analogRead(WATERING_AMOUNT_POTI_PIN);
+
+  float wateringModifier = potiValue / 1024.0;
+
+  int newWateringAmountTime = (int)(WATERING_AMOUNT_TIME * wateringModifier);
+
+  if (newWateringAmountTime != wateringAmountTime)
+  {
+    wateringAmountTime = newWateringAmountTime;
+
+    Serial.print("Adjust Watering amount to ");
+    Serial.print(wateringAmountTime);
+    Serial.println("ms per cycle");
+  }
+}
+
 void waterPlant() {
   int wateringCycles = 0;
 
@@ -133,7 +151,7 @@ void waterPlant() {
 
   while (plantNeedsWater() && !tankLevelWasCritical) {
     setStatusColor(255, 255, 255);
-    openWaterValve(WATERING_AMOUNT_TIME);
+    openWaterValve(wateringAmountTime);
     setStatusColor(255, 255, 0);
 
     Serial.print("Waiting ");
@@ -142,6 +160,8 @@ void waterPlant() {
     delay(WATER_SPREAD_DELAY);
 
     checkTank();
+    manualAdjustWateringAmount();
+    readMoistureLevelThreshold();
     setStatusColor(255, 255, 0);
 
     wateringCycles += 1;
@@ -169,9 +189,9 @@ void waterPlant() {
 }
 
 void scheduleNextCheck() {
-  nextCheckDateTime = rtc.now() + CHECK_INTERVAL;
+  nextCheckDateTime = millis() + CHECK_INTERVAL;
   Serial.print("Next Check is scheduled for ");
-  printDate(nextCheckDateTime);
+  Serial.println(nextCheckDateTime);
 }
 
 
@@ -188,28 +208,14 @@ void setup() {
   Serial.print("Starting Plant Watering System v");
   Serial.println(VERSION);
 
-  Serial.println("Starting RTC...");
-  if (!rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
-
-  if (!rtc.isrunning()) {
-    Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
-  Serial.println("RTC started.");
-
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(MOISTURE_SENSOR_PIN, INPUT);
   pinMode(TANK_LEVEL_SENSOR_PIN, INPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(VALVE_PIN, OUTPUT);
 
-  nextCheckDateTime = rtc.now();
+  nextCheckDateTime = millis();
 
   setStatusColor(0, 0, 0);
 
@@ -218,13 +224,27 @@ void setup() {
 }
 
 bool isTimeForNextCheck() {
-  TimeSpan timeUntilNextCheck = getTimeUntilNextCheck();
-  return timeUntilNextCheck.totalseconds() <= 0;
+  float timeUntilNextCheck = getTimeUntilNextCheck();
+  return timeUntilNextCheck <= 0;
 }
 
-void loop() {
-  //openWaterValve(1000);
-  //return;
+bool wateringButtonIsPressed()
+{
+  return digitalRead(BUTTON_PIN) == LOW;
+}
+
+void loop()
+{
+  if (wateringButtonIsPressed())
+  {
+    Serial.println("Manual override watering initiated.");
+    openWaterValve(500);
+    return;
+  }
+
+  manualAdjustWateringAmount();
+  readMoistureLevelThreshold();
+
   checkTank();
 
   if (!tankLevelWasCritical) {
